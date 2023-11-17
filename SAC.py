b6dca64e62552edeba5b0833a178e6ba3739f580
XinJingHao <75819608+XinJingHao@users.noreply.github.com>
2023-11-17 19:45:05 +0800

    Add files via upload

--- SAC.py
+++ SAC.py
@@ -1,136 +1,51 @@
-import copy
+from utils import Actor, Double_Q_Critic
+import torch.nn.functional as F
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions import Normal
-import math
-from ReplayBuffer import device
+import copy
 
 
-def build_net(layer_shape, activation, output_activation):
-	'''Build net with for loop'''
-	layers = []
-	for j in range(len(layer_shape)-1):
-		act = activation if j < len(layer_shape)-2 else output_activation
-		layers += [nn.Linear(layer_shape[j], layer_shape[j+1]), act()]
-	return nn.Sequential(*layers)
+class SAC_countinuous():
+	def __init__(self, **kwargs):
+		# Init hyperparameters for agent, just like "self.gamma = opt.gamma, self.lambd = opt.lambd, ..."
+		self.__dict__.update(kwargs)
+		self.tau = 0.005
 
+		self.actor = Actor(self.state_dim, self.action_dim, (self.net_width,self.net_width)).to(self.dvc)
+		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.a_lr)
 
-class Actor(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape, h_acti=nn.ReLU, o_acti=nn.ReLU):
-		super(Actor, self).__init__()
-
-		layers = [state_dim] + list(hid_shape)
-		self.a_net = build_net(layers, h_acti, o_acti)
-		self.mu_layer = nn.Linear(layers[-1], action_dim)
-		self.log_std_layer = nn.Linear(layers[-1], action_dim)
-
-		self.LOG_STD_MAX = 2
-		self.LOG_STD_MIN = -20
-
-
-	def forward(self, state, deterministic=False, with_logprob=True):
-		'''Network with Enforcing Action Bounds'''
-		net_out = self.a_net(state)
-		mu = self.mu_layer(net_out)
-		log_std = self.log_std_layer(net_out)
-		log_std = torch.clamp(log_std, self.LOG_STD_MIN, self.LOG_STD_MAX)  #总感觉这里clamp不利于学习
-		std = torch.exp(log_std)
-		dist = Normal(mu, std)
-
-		if deterministic: u = mu
-		else: u = dist.rsample() #'''reparameterization trick of Gaussian'''#
-		a = torch.tanh(u)
-
-		if with_logprob:
-			# get probability density of logp_pi_a from probability density of u, which is given by the original paper.
-			# logp_pi_a = (dist.log_prob(u) - torch.log(1 - a.pow(2) + 1e-6)).sum(dim=1, keepdim=True)
-
-			# Derive from the above equation. No a, thus no tanh(h), thus less gradient vanish and more stable.
-			logp_pi_a = dist.log_prob(u).sum(axis=1, keepdim=True) - (2 * (np.log(2) - u - F.softplus(-2 * u))).sum(axis=1, keepdim=True)
-		else:
-			logp_pi_a = None
-
-		return a, logp_pi_a
-
-
-
-class Q_Critic(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape):
-		super(Q_Critic, self).__init__()
-		layers = [state_dim + action_dim] + list(hid_shape) + [1]
-
-		self.Q_1 = build_net(layers, nn.ReLU, nn.Identity)
-		self.Q_2 = build_net(layers, nn.ReLU, nn.Identity)
-
-
-	def forward(self, state, action):
-		sa = torch.cat([state, action], 1)
-		q1 = self.Q_1(sa)
-		q2 = self.Q_2(sa)
-		return q1, q2
-
-
-
-class SAC_Agent(object):
-	def __init__(
-		self,
-		state_dim,
-		action_dim,
-		gamma=0.99,
-		hid_shape=(256,256),
-		a_lr=3e-4,
-		c_lr=3e-4,
-		batch_size = 256,
-		alpha = 0.2,
-		adaptive_alpha = True
-	):
-
-		self.actor = Actor(state_dim, action_dim, hid_shape).to(device)
-		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=a_lr)
-
-		self.q_critic = Q_Critic(state_dim, action_dim, hid_shape).to(device)
-		self.q_critic_optimizer = torch.optim.Adam(self.q_critic.parameters(), lr=c_lr)
+		self.q_critic = Double_Q_Critic(self.state_dim, self.action_dim, (self.net_width,self.net_width)).to(self.dvc)
+		self.q_critic_optimizer = torch.optim.Adam(self.q_critic.parameters(), lr=self.c_lr)
 		self.q_critic_target = copy.deepcopy(self.q_critic)
 		# Freeze target networks with respect to optimizers (only update via polyak averaging)
 		for p in self.q_critic_target.parameters():
 			p.requires_grad = False
 
-		self.action_dim = action_dim
-		self.gamma = gamma
-		self.tau = 0.005
-		self.batch_size = batch_size
+		self.replay_buffer = ReplayBuffer(self.state_dim, self.action_dim, max_size=int(1e6), dvc=self.dvc)
 
-		self.alpha = alpha
-		self.adaptive_alpha = adaptive_alpha
-		if adaptive_alpha:
+		if self.adaptive_alpha:
 			# Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
-			self.target_entropy = torch.tensor(-action_dim, dtype=float, requires_grad=True, device=device)
-			# We learn log_alpha instead of alpha to ensure exp(log_alpha)=alpha>0
-			self.log_alpha = torch.tensor(np.log(alpha), dtype=float, requires_grad=True, device=device)
-			self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=c_lr)
+			self.target_entropy = torch.tensor(-self.action_dim, dtype=float, requires_grad=True, device=self.dvc)
+			# We learn log_alpha instead of alpha to ensure alpha>0
+			self.log_alpha = torch.tensor(np.log(self.alpha), dtype=float, requires_grad=True, device=self.dvc)
+			self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=self.c_lr)
 
-
-
-	def select_action(self, state, deterministic, with_logprob=False):
+	def select_action(self, state, deterministic):
 		# only used when interact with the env
 		with torch.no_grad():
-			state = torch.FloatTensor(state.reshape(1, -1)).to(device)
-			a, _ = self.actor(state, deterministic, with_logprob)
-		return a.cpu().numpy().flatten()
+			state = torch.FloatTensor(state[np.newaxis,:]).to(self.dvc)
+			a, _ = self.actor(state, deterministic, with_logprob=False)
+		return a.cpu().numpy()[0]
 
-
-
-	def train(self,replay_buffer):
-		s, a, r, s_prime, dead_mask = replay_buffer.sample(self.batch_size)
+	def train(self,):
+		s, a, r, s_next, dw = self.replay_buffer.sample(self.batch_size)
 
 		#----------------------------- ↓↓↓↓↓ Update Q Net ↓↓↓↓↓ ------------------------------#
 		with torch.no_grad():
-			a_prime, log_pi_a_prime = self.actor(s_prime)
-			target_Q1, target_Q2 = self.q_critic_target(s_prime, a_prime)
+			a_next, log_pi_a_next = self.actor(s_next, deterministic=False, with_logprob=True)
+			target_Q1, target_Q2 = self.q_critic_target(s_next, a_next)
 			target_Q = torch.min(target_Q1, target_Q2)
-			target_Q = r + (1 - dead_mask) * self.gamma * (target_Q - self.alpha * log_pi_a_prime) #Dead or Done is tackled by Randombuffer
+			target_Q = r + (~dw) * self.gamma * (target_Q - self.alpha * log_pi_a_next) #Dead or Done is tackled by Randombuffer
 
 		# Get current Q estimates
 		current_Q1, current_Q2 = self.q_critic(s, a)
@@ -141,12 +56,10 @@ class SAC_Agent(object):
 		self.q_critic_optimizer.step()
 
 		#----------------------------- ↓↓↓↓↓ Update Actor Net ↓↓↓↓↓ ------------------------------#
-		# Freeze Q-networks so you don't waste computational effort
-		# computing gradients for them during the policy learning step.
-		for params in self.q_critic.parameters():
-			params.requires_grad = 	False
+		# Freeze critic so you don't waste computational effort computing gradients for them when update actor
+		for params in self.q_critic.parameters(): params.requires_grad = False
 
-		a, log_pi_a = self.actor(s)
+		a, log_pi_a = self.actor(s, deterministic=False, with_logprob=True)
 		current_Q1, current_Q2 = self.q_critic(s, a)
 		Q = torch.min(current_Q1, current_Q2)
 
@@ -155,12 +68,11 @@ class SAC_Agent(object):
 		a_loss.backward()
 		self.actor_optimizer.step()
 
-		for params in self.q_critic.parameters():
-			params.requires_grad = 	True
+		for params in self.q_critic.parameters(): params.requires_grad = True
+
 		#----------------------------- ↓↓↓↓↓ Update alpha ↓↓↓↓↓ ------------------------------#
 		if self.adaptive_alpha:
-			# we optimize log_alpha instead of aplha, which is aimed to force alpha = exp(log_alpha)> 0
-			# if we optimize aplpha directly, alpha might be < 0, which will lead to minimun entropy.
+			# We learn log_alpha instead of alpha to ensure alpha>0
 			alpha_loss = -(self.log_alpha * (log_pi_a + self.target_entropy).detach()).mean()
 			self.alpha_optim.zero_grad()
 			alpha_loss.backward()
@@ -171,22 +83,39 @@ class SAC_Agent(object):
 		for param, target_param in zip(self.q_critic.parameters(), self.q_critic_target.parameters()):
 			target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
+	def save(self,EnvName, timestep):
+		torch.save(self.actor.state_dict(), "./model/{}_actor{}.pth".format(EnvName,timestep))
+		torch.save(self.q_critic.state_dict(), "./model/{}_q_critic{}.pth".format(EnvName,timestep))
+
+	def load(self,EnvName, timestep):
+		self.actor.load_state_dict(torch.load("./model/{}_actor{}.pth".format(EnvName, timestep)))
+		self.q_critic.load_state_dict(torch.load("./model/{}_q_critic{}.pth".format(EnvName, timestep)))
 
 
-	def save(self,episode):
-		torch.save(self.actor.state_dict(), "./model/sac_actor{}.pth".format(episode))
-		torch.save(self.q_critic.state_dict(), "./model/sac_q_critic{}.pth".format(episode))
+class ReplayBuffer():
+	def __init__(self, state_dim, action_dim, max_size, dvc):
+		self.max_size = max_size
+		self.dvc = dvc
+		self.ptr = 0
+		self.size = 0
 
+		self.s = torch.zeros((max_size, state_dim) ,dtype=torch.float,device=self.dvc)
+		self.a = torch.zeros((max_size, action_dim) ,dtype=torch.float,device=self.dvc)
+		self.r = torch.zeros((max_size, 1) ,dtype=torch.float,device=self.dvc)
+		self.s_next = torch.zeros((max_size, state_dim) ,dtype=torch.float,device=self.dvc)
+		self.dw = torch.zeros((max_size, 1) ,dtype=torch.bool,device=self.dvc)
 
-	def load(self,episode):
-		self.actor.load_state_dict(torch.load("./model/sac_actor{}.pth".format(episode)))
-		self.q_critic.load_state_dict(torch.load("./model/sac_q_critic{}.pth".format(episode)))
+	def add(self, s, a, r, s_next, dw):
+		#每次只放入一个时刻的数据
+		self.s[self.ptr] = torch.from_numpy(s).to(self.dvc)
+		self.a[self.ptr] = torch.from_numpy(a).to(self.dvc) # Note that a is numpy.array
+		self.r[self.ptr] = r
+		self.s_next[self.ptr] = torch.from_numpy(s_next).to(self.dvc)
+		self.dw[self.ptr] = dw
 
+		self.ptr = (self.ptr + 1) % self.max_size #存满了又重头开始存
+		self.size = min(self.size + 1, self.max_size)
 
-
-
-
-
-
-
-
+	def sample(self, batch_size):
+		ind = torch.randint(0, self.size, device=self.dvc, size=(batch_size,))
+		return self.s[ind], self.a[ind], self.r[ind], self.s_next[ind], self.dw[ind]
--- main.py
+++ main.py
@@ -1,189 +1,121 @@
-import numpy as np
-import torch
-import gym
-from SAC import SAC_Agent
-from ReplayBuffer import RandomBuffer, device
-from torch.utils.tensorboard import SummaryWriter
+from utils import str2bool, evaluate_policy, Action_adapter, Action_adapter_reverse, Reward_adapter
 from datetime import datetime
+from SAC import SAC_countinuous
+import gymnasium as gym
 import os, shutil
 import argparse
-from Adapter import *
+import torch
 
-def str2bool(v):
-    '''transfer str to bool for argparse'''
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'True','true','TRUE', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'False','false','FALSE', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 '''Hyperparameter Setting'''
 parser = argparse.ArgumentParser()
-parser.add_argument('--EnvIdex', type=int, default=3, help='BWv3, BWHv3, Lch_Cv2, PV0, Humanv2, HCv2')
-parser.add_argument('--write', type=str2bool, default=True, help='Use SummaryWriter to record the training')
+parser.add_argument('--dvc', type=str, default='cuda', help='running device: cuda or cpu')
+parser.add_argument('--EnvIdex', type=int, default=0, help='PV1, Lch_Cv2, Humanv4, HCv4, BWv3, BWHv3')
+parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
 parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
-parser.add_argument('--ModelIdex', type=int, default=2250000, help='which model to load')
-parser.add_argument('--seed', type=int, default=1, help='random seed')
+parser.add_argument('--ModelIdex', type=int, default=100, help='which model to load')
 
-parser.add_argument('--total_steps', type=int, default=int(5e6), help='Max training steps')
-parser.add_argument('--save_interval', type=int, default=int(1e4), help='Model saving interval, in steps.')
-parser.add_argument('--eval_interval', type=int, default=int(1e3), help='Model evaluating interval, in stpes.')
-parser.add_argument('--eval_turn', type=int, default=3, help='Model evaluating times, in episode.')
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--Max_train_steps', type=int, default=int(5e6), help='Max training steps')
+parser.add_argument('--save_interval', type=int, default=int(100e3), help='Model saving interval, in steps.')
+parser.add_argument('--eval_interval', type=int, default=int(2.5e3), help='Model evaluating interval, in steps.')
 parser.add_argument('--update_every', type=int, default=50, help='Training Fraquency, in stpes')
+
 parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
-parser.add_argument('--net_width', type=int, default=256, help='Hidden net width')
+parser.add_argument('--net_width', type=int, default=256, help='Hidden net width, s_dim-400-300-a_dim')
 parser.add_argument('--a_lr', type=float, default=3e-4, help='Learning rate of actor')
 parser.add_argument('--c_lr', type=float, default=3e-4, help='Learning rate of critic')
-parser.add_argument('--batch_size', type=int, default=256, help='Batch Size')
+parser.add_argument('--batch_size', type=int, default=256, help='batch_size of training')
 parser.add_argument('--alpha', type=float, default=0.12, help='Entropy coefficient')
 parser.add_argument('--adaptive_alpha', type=str2bool, default=True, help='Use adaptive_alpha or Not')
 opt = parser.parse_args()
+opt.dvc = torch.device(opt.dvc) # from str to torch.device
 print(opt)
 
 
-def evaluate_policy(env, model, render, steps_per_epoch, max_action, EnvIdex):
-    scores = 0
-    turns = opt.eval_turn
-    for j in range(turns):
-        s, done, ep_r = env.reset(), False, 0
-        while not done:
-            # Take deterministic actions at test time
-            a = model.select_action(s, deterministic=True, with_logprob=False)
-            act = Action_adapter(a, max_action)  # [0,1] to [-max,max]
-            s_prime, r, done, info = env.step(act)
-            # r = Reward_adapter(r, EnvIdex)
-            ep_r += r
-            s = s_prime
-            if render:
-                env.render()
-        # print(ep_r)
-        scores += ep_r
-    return scores/turns
-
 def main():
+    EnvName = ['Pendulum-v1','LunarLanderContinuous-v2','Humanoid-v4','HalfCheetah-v4','BipedalWalker-v3','BipedalWalkerHardcore-v3']
+    BrifEnvName = ['PV1', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
 
-    write = opt.write   #Use SummaryWriter to record the training.
-    render = opt.render
+    # Build Env
+    env = gym.make(EnvName[opt.EnvIdex], render_mode = "human" if opt.render else None)
+    eval_env = gym.make(EnvName[opt.EnvIdex])
+    opt.state_dim = env.observation_space.shape[0]
+    opt.action_dim = env.action_space.shape[0]
+    opt.max_action = float(env.action_space.high[0])   #remark: action space【-max,max】
+    opt.max_e_steps = env._max_episode_steps
+    print(f'Env:{EnvName[opt.EnvIdex]}  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
+          f'max_a:{opt.max_action}  min_a:{env.action_space.low[0]}  max_e_steps:{opt.max_e_steps}')
 
-    # Env config:
-    EnvName = ['BipedalWalker-v3','BipedalWalkerHardcore-v3','LunarLanderContinuous-v2','Pendulum-v0','Humanoid-v2','HalfCheetah-v2']
-    BriefEnvName = ['BWv3', 'BWHv3', 'Lch_Cv2', 'PV0', 'Humanv2', 'HCv2']
-    Env_With_Dead = [True, True, True, False, True, False]
-    EnvIdex = opt.EnvIdex
-    env_with_Dead = Env_With_Dead[EnvIdex]
-    #Env like 'LunarLanderContinuous-v2' is with Dead Signal. Important!
-    #Env like 'Pendulum-v0' is without Dead Signal.
-    env = gym.make(EnvName[EnvIdex])
-    eval_env = gym.make(EnvName[EnvIdex])
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
-    max_action = float(env.action_space.high[0])
-    steps_per_epoch = env._max_episode_steps
-    print('Env:',EnvName[EnvIdex],'  state_dim:',state_dim,'  action_dim:',action_dim,
-          '  max_a:',max_action,'  min_a:',env.action_space.low[0], 'max_episode_steps', steps_per_epoch)
+    # Seed Everything
+    env_seed = opt.seed
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed(opt.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    print("Random Seed: {}".format(opt.seed))
 
-    #Interaction config:
-    start_steps = 5*steps_per_epoch #in steps
-    update_after = 2*steps_per_epoch #in steps
-    update_every = opt.update_every
-    total_steps = opt.total_steps
-    eval_interval = opt.eval_interval  #in steps
-    save_interval = opt.save_interval  #in steps
-
-    #Random seed config:
-    random_seed = opt.seed
-    print("Random Seed: {}".format(random_seed))
-    torch.manual_seed(random_seed)
-    env.seed(random_seed)
-    eval_env.seed(random_seed)
-    np.random.seed(random_seed)
-
-    #SummaryWriter config:
-    if write:
+    # Build SummaryWriter to record training curves
+    if opt.write:
+        from torch.utils.tensorboard import SummaryWriter
         timenow = str(datetime.now())[0:-10]
         timenow = ' ' + timenow[0:13] + '_' + timenow[-2::]
-        writepath = 'runs/SAC_{}'.format(BriefEnvName[EnvIdex]) + timenow
+        writepath = 'runs/{}'.format(BrifEnvName[opt.EnvIdex]) + timenow
         if os.path.exists(writepath): shutil.rmtree(writepath)
         writer = SummaryWriter(log_dir=writepath)
 
 
-
-    #Model hyperparameter config:
-    kwargs = {
-        "state_dim": state_dim,
-        "action_dim": action_dim,
-        "gamma": opt.gamma,
-        "hid_shape": (opt.net_width,opt.net_width),
-        "a_lr": opt.a_lr,
-        "c_lr": opt.c_lr,
-        "batch_size":opt.batch_size,
-        "alpha":opt.alpha,
-        "adaptive_alpha":opt.adaptive_alpha
-    }
-
-
-    model = SAC_Agent(**kwargs)
+    # Build DRL model
     if not os.path.exists('model'): os.mkdir('model')
-    if opt.Loadmodel: model.load(opt.ModelIdex)
+    agent = SAC_countinuous(**vars(opt)) # var: transfer argparse to dictionary
+    if opt.Loadmodel: agent.load(BrifEnvName[opt.EnvIdex], opt.ModelIdex)
 
-    replay_buffer = RandomBuffer(state_dim, action_dim, env_with_Dead, max_size=int(1e6))
-
-    if render:
-        average_reward = evaluate_policy(env, model, render, steps_per_epoch, max_action, EnvIdex)
-        print('Average Reward:', average_reward)
+    if opt.render:
+        while True:
+            score = evaluate_policy(env, agent, turns=1)
+            print('EnvName:', BrifEnvName[opt.EnvIdex], 'score:', score)
     else:
-        s, done, current_steps = env.reset(), False, 0
-        for t in range(total_steps):
-            current_steps += 1
+        total_steps = 0
+        while total_steps < opt.Max_train_steps:
+            s, info = env.reset(seed=env_seed)  # Do not use opt.seed directly, or it can overfit to opt.seed
+            env_seed += 1
+            done = False
+
             '''Interact & trian'''
+            while not done:
+                if total_steps < (5*opt.max_e_steps):
+                    act = env.action_space.sample()  # act∈[-max,max]
+                    a = Action_adapter_reverse(act, opt.max_action)  # a∈[-1,1]
+                else:
+                    a = agent.select_action(s, deterministic=False)  # a∈[-1,1]
+                    act = Action_adapter(a, opt.max_action)  # act∈[-max,max]
+                s_next, r, dw, tr, info = env.step(act)  # dw: dead&win; tr: truncated
+                r = Reward_adapter(r, opt.EnvIdex)
+                done = (dw or tr)
 
-            if t < start_steps:
-                #Random explore for start_steps
-                act = env.action_space.sample() #act∈[-max,max]
-                a = Action_adapter_reverse(act,max_action) #a∈[-1,1]
-            else:
-                a = model.select_action(s, deterministic=False, with_logprob=False) #a∈[-1,1]
-                act = Action_adapter(a,max_action) #act∈[-max,max]
+                agent.replay_buffer.add(s, a, r, s_next, dw)
+                s = s_next
+                total_steps += 1
 
-            s_prime, r, done, info = env.step(act)
-            dead = Done_adapter(r, done, current_steps, EnvIdex)
-            r = Reward_adapter(r, EnvIdex)
-            replay_buffer.add(s, a, r, s_prime, dead)
-            s = s_prime
+                '''train if it's time'''
+                # train 50 times every 50 steps rather than 1 training per step. Better!
+                if (total_steps >= 2*opt.max_e_steps) and (total_steps % opt.update_every == 0):
+                    for j in range(opt.update_every):
+                        agent.train()
 
+                '''record & log'''
+                if total_steps % opt.eval_interval == 0:
+                    ep_r = evaluate_policy(eval_env, agent, turns=3)
+                    if opt.write: writer.add_scalar('ep_r', ep_r, global_step=total_steps)
+                    print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
 
-            # 50 environment steps company with 50 gradient steps.
-            # Stabler than 1 environment step company with 1 gradient step.
-            if t >= update_after and t % update_every == 0:
-                for j in range(update_every):
-                    model.train(replay_buffer)
+                '''save model'''
+                if total_steps % opt.save_interval == 0:
+                    agent.save(BrifEnvName[opt.EnvIdex], int(total_steps/1000))
+        env.close()
+        eval_env.close()
 
-            '''save model'''
-            if (t + 1) % save_interval == 0:
-                model.save(t + 1)
-
-            '''record & log'''
-            if (t + 1) % eval_interval == 0:
-                score = evaluate_policy(eval_env, model, False, steps_per_epoch, max_action, EnvIdex)
-                if write:
-                    writer.add_scalar('ep_r', score, global_step=t + 1)
-                    writer.add_scalar('alpha', model.alpha, global_step=t + 1)
-                print('EnvName:', EnvName[EnvIdex], 'seed:', random_seed, 'totalsteps:', t+1, 'score:', score)
-            if done:
-                s, done, current_steps = env.reset(), False, 0
-
-    env.close()
-    eval_env.close()
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
